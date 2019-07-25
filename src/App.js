619252632fcc581fc887fff4b82e52ae5d7ee840
Kevin Hoercher <khoerch@gmail.com>
2019-07-25 17:01:25 -0400

    Start conditional rendering lesson

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import Counter from './state/Counter';
+import React from 'react';
+import Tabs from './state/Tabs';
 
-class App extends Component {
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+]
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Counter count={123} step={2}/>
+        <Tabs tabs={tabsProp}/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RouletteGun from './state-drills/RouletteGun';
-import Bomb from './state-drills/Bomb';
-import HelloWorld from './state-drills/HelloWorld';
+import App from './App';
 import './index.css';
 
-ReactDOM.render(
-    <div>
-        <RouletteGun bulletInChamber={5}/>
-        <Bomb />
-        <HelloWorld />
-    </div>
-    , document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
